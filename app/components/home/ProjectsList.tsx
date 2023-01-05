6299ef7a8ee19b981d7435bc30a0f665a2c7f162
Adam Mikacich <adammikacich@gmail.com>
2023-01-05 15:27:10 -0800

    Add projects

--- app/components/home/ProjectsList.tsx
+++ app/components/home/ProjectsList.tsx
@@ -61,6 +61,154 @@ export default function ProjectsList() {
                 </div>
               </div>
             </article>
+            <article aria-labelledby="project-2" className="py-10 sm:py-12">
+              <div className="lg:px-12">
+                <div className="lg:max-w-4xl">
+                  <div className="mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-0">
+                    <div className="flex flex-col items-start">
+                      <h2
+                        id="project-2"
+                        className="mt-2 text-lg font-bold text-slate-900"
+                      >
+                        ConvoVault, Slack Message Archiving Application
+                      </h2>
+                      <time className="order-first font-mono text-sm leading-7 text-slate-500">
+                        July 2020
+                      </time>
+                      <ul className="mt-1 text-base leading-7 text-slate-700 list-disc list-outside -ml-2">
+                        <li className="pl-2">
+                          Created an open source tool for organizations to store
+                          an archive of their messages on the Slack platform
+                        </li>
+                        <li className="pl-2">
+                          Automatically listens to all public channels and saves
+                          any new messages to a searchable index
+                        </li>
+                        <li className="pl-2">
+                          Users can evoke a command to access the archive and
+                          filter the results
+                        </li>
+                      </ul>
+                      <div className="mt-4 flex items-center gap-4">
+                        <button className="flex items-center text-sm font-bold leading-6 text-sky-500 hover:text-sky-700 active:text-sky-900">
+                          <IoOpenOutline className="w-6 h-6" />
+                          <span className="ml-2">Learn More</span>
+                        </button>
+                        <span
+                          aria-hidden="true"
+                          className="text-sm font-bold text-slate-400"
+                        >
+                          /
+                        </span>
+                        <button className="flex items-center text-sm font-bold leading-6 text-sky-500 hover:text-sky-700 active:text-sky-900">
+                          <IoLogoGithub className="w-6 h-6" />
+                          <span className="ml-2">GitHub</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
+            <article aria-labelledby="project-3" className="py-10 sm:py-12">
+              <div className="lg:px-12">
+                <div className="lg:max-w-4xl">
+                  <div className="mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-0">
+                    <div className="flex flex-col items-start">
+                      <h2
+                        id="project-3"
+                        className="mt-2 text-lg font-bold text-slate-900"
+                      >
+                        Scouting Native, Robotics Mobile Application
+                      </h2>
+                      <time className="order-first font-mono text-sm leading-7 text-slate-500">
+                        June 2021
+                      </time>
+                      <ul className="mt-1 text-base leading-7 text-slate-700 list-disc list-outside -ml-2">
+                        <li className="pl-2">
+                          Built a new alternative to the traditional scantron
+                          system used to record scouting data at robotic
+                          competitions
+                        </li>
+                        <li className="pl-2">
+                          Allows scouters to input data on a tablet and stream
+                          the information to the team’s backend systems in real
+                          time
+                        </li>
+                        <li className="pl-2">
+                          Built using Vue on the React Native platform, which
+                          was later ported to Cordova
+                        </li>
+                      </ul>
+                      <div className="mt-4 flex items-center gap-4">
+                        <button className="flex items-center text-sm font-bold leading-6 text-sky-500 hover:text-sky-700 active:text-sky-900">
+                          <IoOpenOutline className="w-6 h-6" />
+                          <span className="ml-2">Learn More</span>
+                        </button>
+                        <span
+                          aria-hidden="true"
+                          className="text-sm font-bold text-slate-400"
+                        >
+                          /
+                        </span>
+                        <button className="flex items-center text-sm font-bold leading-6 text-sky-500 hover:text-sky-700 active:text-sky-900">
+                          <IoLogoGithub className="w-6 h-6" />
+                          <span className="ml-2">GitHub</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
+            <article aria-labelledby="project-4" className="py-10 sm:py-12">
+              <div className="lg:px-12">
+                <div className="lg:max-w-4xl">
+                  <div className="mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-0">
+                    <div className="flex flex-col items-start">
+                      <h2
+                        id="project-4"
+                        className="mt-2 text-lg font-bold text-slate-900"
+                      >
+                        Pit Display, Interactive Multi-Touch Web Application
+                      </h2>
+                      <time className="order-first font-mono text-sm leading-7 text-slate-500">
+                        April 2022
+                      </time>
+                      <ul className="mt-1 text-base leading-7 text-slate-700 list-disc list-outside -ml-2">
+                        <li className="pl-2">
+                          Developed an interactive display application for a
+                          portrait touch-enabled monitor
+                        </li>
+                        <li className="pl-2">
+                          Enables users to learn about our team and robot
+                          through images and an interactive 3D view
+                        </li>
+                        <li className="pl-2">
+                          Built using HTML, CSS, JavaScript, Vue, and NodeJS
+                        </li>
+                      </ul>
+                      <div className="mt-4 flex items-center gap-4">
+                        <button className="flex items-center text-sm font-bold leading-6 text-sky-500 hover:text-sky-700 active:text-sky-900">
+                          <IoOpenOutline className="w-6 h-6" />
+                          <span className="ml-2">Learn More</span>
+                        </button>
+                        <span
+                          aria-hidden="true"
+                          className="text-sm font-bold text-slate-400"
+                        >
+                          /
+                        </span>
+                        <button className="flex items-center text-sm font-bold leading-6 text-sky-500 hover:text-sky-700 active:text-sky-900">
+                          <IoLogoGithub className="w-6 h-6" />
+                          <span className="ml-2">GitHub</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
           </div>
         </div>
       </div>
