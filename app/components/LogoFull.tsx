78bf1f7710c2744756acd24f958f7a4c9cbdad1f
Adam Mikacich <adammikacich@gmail.com>
2023-01-04 21:11:52 -0800

    Update logo

--- app/components/LogoFull.tsx
+++ app/components/LogoFull.tsx
@@ -9,7 +9,7 @@ export default function NavSection(props: { className?: string }) {
       href="/"
       className={`flex items-center lg:w-64 ${lexend.className} ${props.className}`}
     >
-      <p className="bg-primary-700 text-white p-2 mr-2 font-extrabold rounded-lg text-sm">
+      <p className="text-sky-500 p-2 mr-2 font-extrabold rounded-full text-sm border-sky-500 border-2">
         AM
       </p>
       <p className="self-center text-xl font-normal whitespace-nowrap dark:text-white">
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { IoLogoGithub } from 'react-icons/io5';
 import LogoFull from './LogoFull';
 
 export default function NavBar() {
@@ -5,6 +6,9 @@ export default function NavBar() {
     <header className="sticky top-0 z-50 flex items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8 dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75">
       <div className="relative flex flex-grow basis-0 items-center">
         <LogoFull />
+      </div>
+      <div>
+        <IoLogoGithub className="w-7 h-7 transition-colors text-primary-400 hover:text-primary-300 cursor-pointer" />
       </div>
     </header>
   );
