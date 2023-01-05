f04b898bc4bdfb7a28e19261d4bfa8157a0e170d
Adam Mikacich <adammikacich@gmail.com>
2023-01-04 21:03:31 -0800

    Add content div

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,5 +1,10 @@
 import NavBar from '@/components/NavBar';
 
 export default function Home() {
-  return <NavBar></NavBar>;
+  return (
+    <>
+      <NavBar></NavBar>
+      <div></div>
+    </>
+  );
 }
